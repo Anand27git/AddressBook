359b9d7e897c9c5f7b75d7f625ffecc621860a82
Anand27git <anandkumar256k@gmail.com>
2021-06-21 18:36:25 +0530

    adding AddressBook

--- AddressBook.java
+++ AddressBook.java
@@ -4,7 +4,7 @@ package com.Bridgelab.AddressBook;
  * 
  * @author ANAND
  *  program:AddressBookUC4 
- *  purpose: Delete contact
+ *  purpose:  create AddressBook
  *  
  *
  ***************************/
@@ -158,55 +158,6 @@ public class AddressBook {
 			System.out.println("No such Contact to Delete");
 		}
 	}
-
-	// Main method
-	public static void main(String[] args) {
-
-		System.out.println("Welcome to the Address Book System");
-		int ch = 0;
-		Scanner sc = new Scanner(System.in);
-		// Creating a Object for Main Class
-		AddressBook ab = new AddressBook();
-		System.out.println("welcome and create address books ");
-		AdressBookManage addBookManage = new AdressBookManage();
-		addBookManage.createAddBooks();
-		System.out.println("Successfully created address books");
-		addBookManage.viewAddBooks();
-
-		// Checking a Choice with Switch Statement
-		while (true) {
-			System.out.println("1.Adding Contact \n2.Edit Contact \n3.Delete \n4.Exit");
-			System.out.println("Enter a Your Choice :");
-			ch = sc.nextInt();
-
-			switch (ch) {
-			case 1:
-				AddressBookContacts addressContactDetails = ab.getContactInput();
-				ab.addContact(addressContactDetails);
-
-				System.out.println("Contact Added Successfully");
-				break;
-			case 2:
-				ab.editContact();
-				System.out.println("Contact Updated Successfully");
-				break;
-
-			case 3:
-				ab.deleteContact();
-				System.out.println("Contact deleted Successfully");
-				break;
-
-			case 4:
-				System.out.println("you have choosen choice to exit");
-				return;
-
-			default:
-				System.out.println("Please Enter Valid Choice");
-
-			}
-		}
-
-	}
 //Method to create addressbook uc-6
 public AddressBook addressBookOption() {
 	Scanner sc = new Scanner(System.in);
@@ -214,8 +165,7 @@ public AddressBook addressBookOption() {
 	AddressBook addBook = new AddressBook();
 	boolean runLoop = true;
 	while(runLoop) {
-		System.out.println("Press 1 for adding contact \n "
-				+ "\nPress 2 to edit a contact \nPress 3 to delete a contact \nPress 4 to exit");
+		System.out.println("Press 1 for adding contact\nPress 2 to edit a contact\nPress 3 to delete a contact\nPress 4 to exit");
 		int ch = sc.nextInt();
 		
 		switch(ch) {
