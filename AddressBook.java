7a02c173ed9b52ee253bad8a265458722bebbd4f
Anand27git <anandkumar256k@gmail.com>
2021-07-04 11:37:34 +0530

    Search person By City  and State

--- AddressBook.java
+++ AddressBook.java
@@ -4,7 +4,7 @@ package com.Bridgelab.AddressBook;
  * 
  * @author ANAND
  *  program:AddressBookUC4 
- *  purpose: Duplicate contact
+ *  purpose: Delete contact
  *  
  *
  ***************************/
@@ -16,11 +16,9 @@ import java.util.Scanner;
 
 public class AddressBook {
 
-	private ArrayList<AddressBookContacts> addressContactList;
-    private Map<String, AddressBookContacts> nameToContactDetailsMap;
-	
-	
-	
+	ArrayList<AddressBookContacts> addressContactList;
+	Map<String, AddressBookContacts> nameToContactDetailsMap;
+
 	public AddressBook() {
 		// Initializing ArrayList
 		addressContactList = new ArrayList<AddressBookContacts>();
@@ -158,60 +156,46 @@ public class AddressBook {
 			System.out.println("No such Contact to Delete");
 		}
 	}
-	
-	// method to check duplicate of contacts Uc-7
-	public void duplicateContact() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a person Name:");
-		String firstName = sc.nextLine();
-		for (int i = 0; i < addressContactList.size(); i++) {
-			AddressBookContacts model = addressContactList.get(i);
 
-			if (addressContactList.get(i).getFirstName().equals(firstName)) {
-				System.out.println("Contact Already Exist, Please add another contact");
+	// Method to create addressbook uc-6
+	public AddressBook addressBookOption() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to the address book system. Choose your option");
+		AddressBook addBook = new AddressBook();
+		boolean runLoop = true;
+		while (runLoop) {
+			System.out.println(
+					"Press 1 for adding contact\nPress:2 to edit a contact\nPress:3 to delete a contact\n press 4 to exit");
+			int ch = sc.nextInt();
+
+			switch (ch) {
+			case 1:
+				System.out.println("---- Add contact details ---- ");
+				AddressBookContacts contactPerson = addBook.getContactInput();
+				addBook.addContact(contactPerson);
+				System.out.println("Contact added for " + contactPerson.firstName + " " + contactPerson.lastName);
+				break;
+
+			case 2:
+				System.out.println("---- Editing contacts---");
+				addBook.editContact();
+				break;
+
+			case 3:
+				System.out.println("---- Delete a contact---");
+				addBook.deleteContact();
+				break;
+			
+			case 4:
+				System.out.println("exit");
+				runLoop = false;
+				break;
+
+			default:
+				System.out.println("No correct option chosen");
+
 			}
 		}
+		return addBook;
 	}
-
-
-	//Adressbook uc-6
-	public AddressBook addressBookOption() {
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Welcome to the address book system. Choose your option");
-	AddressBook addBook = new AddressBook();
-	boolean runLoop = true;
-	while(runLoop) {
-		System.out.println("Press 1 for adding contact\nPress 2 to edit a contact\nPress 3 to delete a contact\nPress 4 Duplicate Person Name\n Press 5 to exit");
-		int ch = sc.nextInt();
-		
-		switch(ch) {
-			case 1: System.out.println("---- Add contact details ---- ");
-					AddressBookContacts contactPerson = addBook.getContactInput();
-					addBook.addContact(contactPerson);
-					System.out.println("Contact added for " + contactPerson.firstName + " " + contactPerson.lastName);
-					break;
-					
-			case 2: System.out.println("---- Editing contacts---");
-					addBook.editContact();
-					break;
-			
-			case 3: System.out.println("---- Delete a contact---");
-					addBook.deleteContact();
-					break;
-
-			 case 4: System.out.println("---- Duplicate persons---");
-                                        addBook.duplicateContact();
-                                        break;
-					
-			case 5: System.out.println("exit");
-					runLoop = false;
-					break;
-					
-			default: System.out.println("No correct option chosen");
-				
-			
-		}
-	}
-	return addBook;
-}
 }
--- AdressBookManage.java
+++ AdressBookManage.java
@@ -1,12 +1,15 @@
 package com.Bridgelab.AddressBook;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class AdressBookManage {
 
 	private Map<String, AddressBook> nameToAddressBookMap;
+	List<AddressBookContacts> valuePrinted = null;
 
 	public AdressBookManage() {
 		nameToAddressBookMap = new HashMap<>();
@@ -16,7 +19,7 @@ public class AdressBookManage {
 		nameToAddressBookMap.put(addBookName, addBook);
 	}
 
-	public void createAddBooks() {
+	public boolean createAddBooks() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the num of address books to create");
 		int num = sc.nextInt();
@@ -30,21 +33,72 @@ public class AdressBookManage {
 			addBookObj = addBookObj.addressBookOption();
 			addAddressBook(aBookName, addBookObj);
 		}
+		return false;
 	}
-
+	
+	
 	public void viewAddBooks() {
 		for (int i = 0; i < nameToAddressBookMap.size(); i++) {
 			System.out.println(i + "Name of add. book are : " + nameToAddressBookMap);
 		}
 	}
+	
+	// UC 8 find the person by city
+		public void findPersonByCity(String cityName) 
+		{
+			nameToAddressBookMap.forEach((key, addresBookValue) -> {
+				valuePrinted = addresBookValue.addressContactList.stream()
+									   .filter(n -> n.city.equals(cityName))
+									   .peek( n -> System.out.println("Person name---"+ n.firstName + " "+ n.lastName))
+									   .collect(Collectors.toList());
+			});
+		}
+    // UC 9 Find  the person by state
+		public void findPersonByState(String stateName) 
+		{
+			nameToAddressBookMap.forEach((key, addresBookValue) -> {
+				valuePrinted = addresBookValue.addressContactList.stream()
+									   .filter(n -> n.state.equals(stateName))
+									   .peek( n -> System.out.println("Person name---"+ n.firstName + " "+ n.lastName))
+									   .collect(Collectors.toList());
+				
+			});
+		}
+		
+	
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("welcome and create address books ");
 		AdressBookManage addBookManage = new AdressBookManage();
 		addBookManage.createAddBooks();
+		boolean created = addBookManage.createAddBooks();
 		System.out.println("Successfully created address books");
 		addBookManage.viewAddBooks();
-//			addBookManage.addAddressBook(addBookObj, "firstAddBook");
+		
+		if (created) 
+		{
+			System.out.println("Enter 1 to find by City \nEnter 2 to find by State");
+			int ch = sc.nextInt();
+			sc.nextLine();
+			switch (ch) 
+			{
+				case 1: 
+						System.out.println("Enter the name of City: ");
+						String cityName = sc.nextLine();
+						addBookManage.findPersonByCity(cityName);
+						break;
+				
+				case 2:
+						System.out.println("Enter the name of State: ");
+						String stateName = sc.nextLine();
+						addBookManage.findPersonByState(stateName);
+						break;
+				default:
+					    break;
+			}
+			
+		}
 
 	}
 }
